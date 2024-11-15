fa1e4e7f90b2f8ce6e12578fd6e4e6413220c433
Corwin Dickey <62188881+CorwinDickey@users.noreply.github.com>
2024-11-15 13:16:01 -0600

    added db schema list class

---
+++ api/dotnet/Domain/Schemas.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain
+{
+    /// <summary>
+    /// Defines the DB schemas for categorizing domain objects
+    /// </summary>
+    public class Schemas
+    {
+        /// <summary>
+        /// The User schema, for all things primarily related to users
+        /// </summary>
+        public const string User = "User";
+
+        /// <summary>
+        /// The MetaData schema, for all things that have no specific categorization and could be used in multiple places
+        /// </summary>
+        public const string MetaData = "MetaData";
+
+        /// <summary>
+        /// The Inventory schema, used for all things primarily related to inventory items
+        /// </summary>
+        public const string Inventory = "Inventory";
+    }
+}
