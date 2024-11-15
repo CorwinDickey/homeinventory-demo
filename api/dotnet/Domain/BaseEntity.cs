ec809d8957962897ec98faa7a797129b82c333eb
Corwin Dickey <62188881+CorwinDickey@users.noreply.github.com>
2024-11-15 13:13:17 -0600

    added the base entity class

---
+++ api/dotnet/Domain/BaseEntity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    /// <summary>
+    /// Base attributes required for an entity
+    /// </summary>
+    public abstract class BaseEntity
+    {
+        /// <summary>
+        /// The DB Id of the entity
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The timestamp the entity was created
+        /// </summary>
+        public DateTimeOffset CreatedDate { get; set; }
+
+        /// <summary>
+        /// The DB Id of the user that created the entity
+        /// </summary>
+        public int CreatedUserId { get; set; }
+
+        /// <summary>
+        /// The DB Id of the parent entity if this entity is a modification of an existing entity (for auditing/history purposes)
+        /// </summary>
+        public int? ParentEntityId { get; set; }
+
+        /// <summary>
+        /// The timestamp the entity was "deleted" at
+        /// </summary>
+        public DateTimeOffset? DeletedDate { get; set; }
+
+        /// <summary>
+        /// The DB Id of the user who "deleted" the entity
+        /// </summary>
+        public int? DeletedUserId { get; set; }
+    }
+}
