eb4259adb02cad8079f90cda602641b512aee062
Corwin Dickey <62188881+CorwinDickey@users.noreply.github.com>
2024-11-18 15:53:23 -0600

    added entity property and cleaned up usings

--- api/dotnet/Repository/RepoContext.cs
+++ api/dotnet/Repository/RepoContext.cs
@@ -1,15 +1,10 @@
-﻿using Domain;
+﻿using System.Security.Claims;
+using Domain;
 using Domain.Inventory;
 using Domain.MetaData;
 using Domain.User;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repository
 {
