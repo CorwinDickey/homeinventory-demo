d1eeb45734aaa7f604809406cf85766fa2707938
Corwin Dickey <62188881+CorwinDickey@users.noreply.github.com>
2025-03-26 11:12:17 -0500

    pushing up changes

---
+++ api/dotnet/Services/Dtos/InventoryItemDto.cs
@@ -0,0 +1,80 @@
+﻿using Domain.Inventory;
+
+namespace Services.Dtos
+{
+    /// <summary>
+    /// DTO for an <see cref="InventoryItem"/>
+    /// </summary>
+    public class InventoryItemDto
+    {
+        /// <summary>
+        /// DB Id of the <see cref="InventoryItem"/> if it already exists in the db, otherwise null.
+        /// </summary>
+        public int? Id { get; set; }
+
+        /// <summary>
+        /// ISO 8601 string of the date the item was added to the inventory.
+        /// </summary>
+        public string CreatedDate { get; set; }
+
+        /// <summary>
+        /// DB Id of the user who created the item in the DB.
+        /// </summary>
+        public int? CreatedUserId { get; set; }
+
+        /// <summary>
+        /// DB Id of the entity this entity is based on, if this entity is a modification of a previous entry. Null if newly created entity.
+        /// </summary>
+        public int? ParentId { get; set; }
+
+        /// <summary>
+        /// ISO 8601 string of the date the entity was deleted by the user, if any.
+        /// </summary>
+        public string DeletedDate { get; set; }
+
+        /// <summary>
+        /// DB Id of the user who deleted the entity, if any.
+        /// </summary>
+        public int? DeletedUserId { get; set; }
+
+        /// <summary>
+        /// ISO 8601 string of the date the item was purchased, if known.
+        /// </summary>
+        public string DatePurchased { get; set; }
+
+        /// <summary>
+        /// Price the item was purchased for, if known.
+        /// </summary>
+        public decimal? PurchasePrice { get; set; }
+
+        /// <summary>
+        /// Location the item was purchased at, if known.
+        /// </summary>
+        public string PurchaseLocation { get; set; }
+
+        /// <summary>
+        /// Name of the item.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Model of the item, if any.
+        /// </summary>
+        public string Model { get; set; }
+
+        /// <summary>
+        /// Serial number of the item, if any.
+        /// </summary>
+        public string SerialNumber { get; set; }
+
+        /// <summary>
+        /// Brand of the item, if known.
+        /// </summary>
+        public string Brand { get; set; }
+
+        /// <summary>
+        /// Additional notes about the item.
+        /// </summary>
+        public string Notes { get; set; }
+    }
+}
---
+++ api/dotnet/Services/Interfaces/IInventoryItemService.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Inventory;
+using Domain.User;
+using Services.Dtos;
+
+namespace Services.Interfaces
+{
+    /// <summary>
+    /// Defines the methods available to work with <see cref="InventoryItem"/>s in the application.
+    /// </summary>
+    public interface IInventoryItemService
+    {
+        /// <summary>
+        /// Creates a new inventory item and adds it to the DB.
+        /// </summary>
+        /// <param name="user">User creating the item.</param>
+        /// <param name="itemDto">Data model containing information about the item to be created.</param>
+        /// <returns><see cref="InventoryItemDto"/> with metadata after creation.</returns>
+        InventoryItemDto CreateInventoryItem(
+            ApplicationUser user, InventoryItemDto itemDto);
+
+        /// <summary>
+        /// Creates a new entity with updated data, maintains original data for history and auditing.
+        /// </summary>
+        /// <param name="user">User updating the item.</param>
+        /// <param name="itemDto">Data model containing updated item information.</param>
+        /// <returns><see cref="InventoryItemDto"/> with data for new record and Id of original entity for historical data/auditing.</returns>
+        InventoryItemDto UpdateInventoryItem(
+            ApplicationUser user, InventoryItemDto itemDto);
+
+        /// <summary>
+        /// Creates a new entity with deleted properties set, maintains original data for history and auditing.
+        /// </summary>
+        /// <param name="user">User deleting the item.</param>
+        /// <param name="id">DB Id of the item being deleted.</param>
+        void DeleteInventoryItem(
+            ApplicationUser user, int id);
+    }
+}
